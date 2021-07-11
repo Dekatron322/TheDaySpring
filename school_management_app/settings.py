97e178f4633993ffc1c2a5a618261553280fa1e2
Dekatron322 <62252129+Dekatron322@users.noreply.github.com>
2021-07-11 22:00:17 +0100

    error 404

--- school_management_app/settings.py
+++ school_management_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6#9(g$_pmm*tpr9arw&sfwm@=71+c66&fg7vxo^h!jwq4gcl-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
